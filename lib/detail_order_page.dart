cd41d81524fff9ff98eae8731f65347e096d949f
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-23 18:30:10 +0700

    tambah detail order dan navigasi selesai

--- lib/detail_order_page.dart
+++ lib/detail_order_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:login/home_page.dart';
  
  class DetailOrderPage extends StatelessWidget {
    final String makanan;
@@ -17,6 +18,32 @@ import 'package:flutter/material.dart';
  
    @override
    Widget build(BuildContext context) {
-     return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text('Detail Order Page')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Food Order: $makanan'),
+            Text('Drink Order: $minuman'),
+            Text('Food QTY Order: $jumlahMakanan'),
+            Text('Drink QTY Order: $jumlahMinuman'),
+            Text('Total Price: $totalHarga'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                  (route) => false,
+                );
+              },
+              child: Text('Finish Order'),
+            ),
+          ],
+        ),
+      ),
+    );
    }
  }
