34370d45fef89aa42c66df54b40bf82aa9dddf24
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-23 18:00:34 +0700

    add: OrderPage dengan controller

---
+++ lib/order_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+ 
+ class OrderPage extends StatefulWidget {
+   const OrderPage({super.key});
+ 
+   @override
+   State<OrderPage> createState() => _OrderPageState();
+ }
+ 
+ class _OrderPageState extends State<OrderPage> {
+   final TextEditingController makananController = TextEditingController();
+   final TextEditingController minumanController = TextEditingController();
+   final TextEditingController jumlahMakananController = TextEditingController();
+   final TextEditingController jumlahMinumanController = TextEditingController();
+   int totalHarga = 0;
+   
+   @override
+   Widget build(BuildContext context) {
+     return const Placeholder();
+   }
+ }
