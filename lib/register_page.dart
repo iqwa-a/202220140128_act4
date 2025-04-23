7ad477f26c0768162382265b1a46e016e83bd93d
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-23 17:46:58 +0700

    create Register_page

---
+++ lib/register_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+ 
+ class RegisterPage extends StatefulWidget {
+   const RegisterPage({super.key});
+ 
+   @override
+   State<RegisterPage> createState() => _RegisterPageState();
+ }
+ 
+ class _RegisterPageState extends State<RegisterPage> {
+   final TextEditingController emailController = TextEditingController();
+   final TextEditingController passwordController = TextEditingController();
+ 
+   @override
+   void initState() {
+     super.initState();
+   }
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       body: Form(
+         child: Padding(
+           padding: const EdgeInsets.all(16.0),
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Text('Register'),
+               TextFormField(
+                 controller: emailController,
+                 decoration: const InputDecoration(labelText: 'Email'),
+                 validator: (value) {
+                   if (value == null || value.isEmpty) {
+                     return 'Please enter your email';
+                   }
+                   return null;
+                 },
+               ),
+               TextFormField(
+                 controller: passwordController,
+                 decoration: const InputDecoration(labelText: 'Password'),
+                 obscureText: true,
+                 validator: (value) {
+                   if (value == null || value.isEmpty) {
+                     return 'Please enter your password';
+                   }
+                   return null;
+                 },
+               ),
+             ],
+           ),
+         ),
+       ),
+     );
+   }
+ }
