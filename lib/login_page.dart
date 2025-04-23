839417410dd7bd68616bb3b47523af286b2a22aa
iqwa1107 <127074183+iqwa1107@users.noreply.github.com>
2025-04-23 17:31:32 +0700

    create login_page

---
+++ lib/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+ 
+ class LoginPage extends StatefulWidget {
+   const LoginPage({super.key});
+ 
+   @override
+   State<LoginPage> createState() => _LoginPageState();
+ }
+ 
+ class _LoginPageState extends State<LoginPage> {
+   final TextEditingController emailController = TextEditingController();
+   final TextEditingController passwordController = TextEditingController();
+ 
+   @override
+   void initState() {
+     super.initState();
+   }
+ 
+   @override
+   Widget build(BuildContext context) {
+     final _formKey = GlobalKey<FormState>();
+     return Scaffold(
+       body: Form(
+         key: _formKey,
+         child: Padding(
+           padding: const EdgeInsets.all(16.0),
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               TextFormField(
+                 controller: emailController,
+                 decoration: const InputDecoration(labelText: 'Email'),
+                 validator: (value) {
+                   if (value == null || value.isEmpty) {
+                     return 'Please enter your email';
+                   }
+                   return null;
+                 },
+               ),
+               TextFormField(
+                 controller: passwordController,
+                 decoration: const InputDecoration(labelText: 'Password'),
+                 obscureText: true,
+                 validator: (value) {
+                   if (value == null || value.isEmpty) {
+                     return 'Please enter your password';
+                   }
+                   return null;
+                 },
+               ),
+             ],
+           ),
+         ),
+       ),
+     );
+   }
+ }
